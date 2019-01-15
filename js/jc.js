0907e5d3d2d1a9c4530d1d38ab3994c20873c32f
sbwffgli <sbwffgli@hotmail.com>
2019-01-15 08:42:17 +0800

    Update jc.js

--- js/jc.js
+++ js/jc.js
@@ -58,20 +58,4 @@ if (myBrowser() == "IE8") {
     location.href="other/ub.htm";
 }
 
-//平台、设备和操作系统
-var system ={
-win : false,
-mac : false,
-xll : false
-};
-//检测平台
-var p = navigator.platform;
-system.win = p.indexOf("Win") == 0;
-system.mac = p.indexOf("Mac") == 0;
-system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);
-//跳转语句，如果是手机访问就自动跳转到自定义页面
-if(system.win||system.mac||system.xll){
-  
-}else{
-window.location.href="http://blog.kemeiba.com";
-}
+
